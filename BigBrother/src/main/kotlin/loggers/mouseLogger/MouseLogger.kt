5c29dac863e331b8cb93243b1f6b6870544a0ea4
trvehazzk3r <dimakrok@bk.ru>
2022-05-03 19:29:42 +0300

    REF: refactor of mouse logger

--- BigBrother/src/main/kotlin/loggers/mouseLogger/MouseLogger.kt
+++ BigBrother/src/main/kotlin/loggers/mouseLogger/MouseLogger.kt
@@ -1,30 +1,29 @@
 package loggers.mouseLogger
 
+import dateFormat.DateFormatter
+import loggers.Logger
 import org.jnativehook.GlobalScreen
 import org.jnativehook.NativeHookException
 import org.jnativehook.mouse.NativeMouseEvent
 import org.jnativehook.mouse.NativeMouseInputListener
 import java.io.File
-import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.system.exitProcess
 
-class MouseLogger(username: String) : NativeMouseInputListener {
+class MouseLogger(username: String) : Logger, NativeMouseInputListener {
 
-    private val mDateFormat = SimpleDateFormat("dd/M/yyyy HH:mm:ss.SSS")
+    private val mPathToFileForMoves = "${System.getProperty("user.dir")}\\data\\${username}_Mouse_Moves.txt"
+    private val mPathToFileForClicks = "${System.getProperty("user.dir")}\\data\\${username}_Mouse_Clicks.txt"
 
-    private val pathToFileForMoves = "${System.getProperty("user.dir")}\\data\\${username}_Mouse_Moves.txt"
-    private val pathToFileForClicks = "${System.getProperty("user.dir")}\\data\\${username}_Mouse_Clicks.txt"
-
-    private var fileForMoves: File = File(pathToFileForMoves)
-    private var fileForClicks: File = File(pathToFileForClicks)
+    private var mFileForMoves: File = File(mPathToFileForMoves)
+    private var mFileForClicks: File = File(mPathToFileForClicks)
 
     private fun setFiles() {
-        if (!fileForMoves.exists()) fileForMoves.createNewFile()
-        if (!fileForClicks.exists()) fileForClicks.createNewFile()
+        if (!mFileForMoves.exists()) mFileForMoves.createNewFile()
+        if (!mFileForClicks.exists()) mFileForClicks.createNewFile()
     }
 
-    init {
+    override fun start() {
         try {
             GlobalScreen.registerNativeHook()
         } catch (ex: NativeHookException) {
@@ -38,13 +37,16 @@ class MouseLogger(username: String) : NativeMouseInputListener {
         GlobalScreen.addNativeMouseMotionListener(this)
     }
 
-    fun stop() {
+    override fun stop() {
         GlobalScreen.removeNativeMouseListener(this)
         GlobalScreen.unregisterNativeHook()
     }
 
     override fun nativeMouseClicked(p0: NativeMouseEvent?) {
-        fileForClicks.appendText("x=${p0!!.x}, y=${p0.y}, key=${p0.button}, timestamp=${mDateFormat.format(Date())}\n")
+        mFileForClicks.appendText(
+            "x=${p0!!.x}, y=${p0.y}, key=${p0.button}," +
+                    " timestamp=${DateFormatter.dateFormat.format(Date())}\n"
+        )
     }
 
     override fun nativeMousePressed(p0: NativeMouseEvent?) {
@@ -56,7 +58,7 @@ class MouseLogger(username: String) : NativeMouseInputListener {
     }
 
     override fun nativeMouseMoved(p0: NativeMouseEvent?) {
-        fileForMoves.appendText("x=${p0!!.x}, y=${p0.y}, timestamp=${mDateFormat.format(Date())}\n")
+        mFileForMoves.appendText("x=${p0!!.x}, y=${p0.y}, timestamp=${DateFormatter.dateFormat.format(Date())}\n")
     }
 
     override fun nativeMouseDragged(p0: NativeMouseEvent?) {
